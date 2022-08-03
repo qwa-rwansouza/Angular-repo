ef1714056c99bb931a12dd1b98069accaefe08aa
rwan.souza <rwan.souza@qwasolucoes.com.br>
2022-08-03 11:12:52 -0300

    validações

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,7 +1,8 @@
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
+import { minusculoValidator } from 'src/app/utils/minusculo.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -18,9 +19,9 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
-      userName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
+      userName: ['', [minusculoValidator]],
       password: [''],
     });
   }
